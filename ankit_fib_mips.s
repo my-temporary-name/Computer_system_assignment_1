df25e791b155939f582af824ede4c1b6a28c6ad2
Ankit Yadav <106139277+my-temporary-name@users.noreply.github.com>
2022-09-03 00:00:34 +0530

    Add files via upload

---
+++ ankit_fib_mips.s
@@ -0,0 +1,125 @@
+	.file	1 "ankit_fib_mips.c"
+	.section .mdebug.abi32
+	.previous
+	.nan	legacy
+	.module	fp=xx
+	.module	nooddspreg
+	.abicalls
+	.text
+	.align	2
+	.globl	fib
+	.set	nomips16
+	.set	nomicromips
+	.ent	fib
+	.type	fib, @function
+fib:
+	.frame	$fp,40,$31		# vars= 0, regs= 3/0, args= 16, gp= 8
+	.mask	0xc0010000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-40
+	sw	$31,36($sp)
+	sw	$fp,32($sp)
+	sw	$16,28($sp)
+	move	$fp,$sp
+	sw	$4,40($fp)
+	lw	$2,40($fp)
+	slt	$2,$2,2
+	beq	$2,$0,$L2
+	nop
+
+	lw	$2,40($fp)
+	.option	pic0
+	b	$L3
+	nop
+
+	.option	pic2
+$L2:
+	lw	$2,40($fp)
+	addiu	$2,$2,-1
+	move	$4,$2
+	.option	pic0
+	jal	fib
+	nop
+
+	.option	pic2
+	move	$16,$2
+	lw	$2,40($fp)
+	addiu	$2,$2,-2
+	move	$4,$2
+	.option	pic0
+	jal	fib
+	nop
+
+	.option	pic2
+	addu	$2,$16,$2
+$L3:
+	move	$sp,$fp
+	lw	$31,36($sp)
+	lw	$fp,32($sp)
+	lw	$16,28($sp)
+	addiu	$sp,$sp,40
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	fib
+	.size	fib, .-fib
+	.rdata
+	.align	2
+$LC0:
+	.ascii	"%d\000"
+	.text
+	.align	2
+	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,40,$31		# vars= 8, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-40
+	sw	$31,36($sp)
+	sw	$fp,32($sp)
+	move	$fp,$sp
+	lui	$28,%hi(__gnu_local_gp)
+	addiu	$28,$28,%lo(__gnu_local_gp)
+	.cprestore	16
+	li	$2,10			# 0xa
+	sw	$2,28($fp)
+	lw	$4,28($fp)
+	.option	pic0
+	jal	fib
+	nop
+
+	.option	pic2
+	lw	$28,16($fp)
+	move	$5,$2
+	lui	$2,%hi($LC0)
+	addiu	$4,$2,%lo($LC0)
+	lw	$2,%call16(printf)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,printf
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	move	$2,$0
+	move	$sp,$fp
+	lw	$31,36($sp)
+	lw	$fp,32($sp)
+	addiu	$sp,$sp,40
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
